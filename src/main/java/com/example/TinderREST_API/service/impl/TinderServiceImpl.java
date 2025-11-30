37d465c8619dabff7f032b44e83157312c32ee9d
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-30 12:24:01 +0530

    Commit

--- src/main/java/com/example/TinderREST_API/service/impl/TinderServiceImpl.java
+++ src/main/java/com/example/TinderREST_API/service/impl/TinderServiceImpl.java
@@ -1,6 +1,48 @@
 package com.example.TinderREST_API.service.impl;
 
-public class TinderServiceImpl {
+import com.example.TinderREST_API.dto.Person;
+import com.example.TinderREST_API.service.TinderService;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TinderServiceImpl implements TinderService {
+    Person amit=new Person("Amit","Gondia","M",20,"B.Sc");
+    Person lokesh=new Person("Lokesh","Nagpur","M",23,"B.tech");
+    Person dimple=new Person("Dimple","Pune","F",25,"B.Com");
+    Person pinky= new Person("Pinky","chennai","F",23,"B.tech");
+
+    List<Person>personList=new ArrayList<>();
+
+    public List<Person> getAllPerson(){
+        personList.add(amit);
+        personList.add(lokesh);
+        personList.add(dimple);
+        personList.add(pinky);
+
+        return personList;
+    }
+
+    @Value("${User.gender")
+    String userGender;
+
+    public List<Person>filterPerson(){
+
+        List<Person>per=new ArrayList<>();
+
+        for(Person obj:personList){
+            if(!userGender.equalsIgnoreCase(obj.getGender())){
+                per.add(obj);
+            }
+        }
+
+        return per;
+    }
+
+
+
 
 
 }
