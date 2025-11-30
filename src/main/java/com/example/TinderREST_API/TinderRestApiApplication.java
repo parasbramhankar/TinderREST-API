80af99684d16cbc4752c8fd351ff04d591d98457
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-30 11:40:55 +0530

    Initial commit

---
+++ src/main/java/com/example/TinderREST_API/TinderRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.TinderREST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinderRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinderRestApiApplication.class, args);
+	}
+
+}
