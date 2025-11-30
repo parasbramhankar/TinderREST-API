44513f843f0acac134e46ef519edd7e4abe57416
parasbramhankar <parasbramhankar.in@gmail.com>
2025-11-30 14:12:41 +0530

    commit

--- src/main/java/com/example/TinderREST_API/service/TinderService.java
+++ src/main/java/com/example/TinderREST_API/service/TinderService.java
@@ -6,5 +6,9 @@ import java.util.List;
 
 public interface TinderService {
   List<Person> getAllPerson();
-  List<Person> filterPerson();
+  List<Person>filterPersonByGender();
+  Person addPerson(Person person);
+  boolean updatePerson(int id, Person person);
+  boolean updateAgeAndEducation(int id, int age, String education);
+  boolean deletePerson(int id);
 }
